fa7815f0f80d6501d43fad6c30930e873f18aa24
PIEOSTASS <hieusur111@gmail.com>
2026-06-28 23:05:00 +0700

    Delete V15__Add_Evening_Surcharge_End_Time.sql

--- V15__Add_Evening_Surcharge_End_Time.sql
+++
@@ -1,2 +0,0 @@
-INSERT IGNORE INTO SystemConfig (config_key, config_value, config_type, description) VALUES
-('evening_surcharge_end_time', '23:59', 'String', 'Giờ kết thúc áp dụng phụ thu buổi tối (HH:mm)');
